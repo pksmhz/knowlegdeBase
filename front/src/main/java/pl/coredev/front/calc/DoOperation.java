1b00384d9b54ab42acb50d4ac58ce6b6151ea4a7
Piotr Strychaluk <pksmhz@wp.pl>
2023-01-02 23:19:02 +0100

    Rabbit removed.

--- front/src/main/java/pl/coredev/front/calc/DoOperation.java
+++ front/src/main/java/pl/coredev/front/calc/DoOperation.java
@@ -1,13 +1,11 @@
 package pl.coredev.front.calc;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
-import pl.coredev.front.rabbit.RabbitSender;
 import pl.coredev.front.types.AllowedResponse;
 import pl.coredev.front.types.OperationRequest;
 import pl.coredev.front.types.OperationType;
@@ -17,9 +15,6 @@ import java.util.Arrays;
 @Slf4j
 @RestController
 public class DoOperation {
-
-    @Autowired
-    RabbitSender sender;
 
     @CrossOrigin
     @PostMapping(value = "/operation")
